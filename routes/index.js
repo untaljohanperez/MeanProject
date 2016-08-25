75d7505ad4dd656ce1ef5ed0cd5ba0593e6cd4ff
UnTalJohanPerez <kedwin85@hotmail.com>
2016-08-25 11:54:44 +0000

    User and admin views

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Process Manager' });
 });
 
+router.get('/view.ejs', function(req, res, next) {
+  res.render('view', { title: 'Process Manager' });
+});
+
 module.exports = router;
